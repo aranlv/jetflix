f21e2dbff2d95cafceda7d5b95bfd07d361b254a
aranlv <109054145+aranlv@users.noreply.github.com>
2023-10-06 17:54:01 +0700

    Delete jetflix directory

--- jetflix/script/header.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('scroll', () => {
-  const header = document.querySelector('header');
-
-  if(window.scrollY > 200){
-    header.classList.add('scrolled')
-  } else{
-    header.classList.remove('scrolled')
-  }
-});
