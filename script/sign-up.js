70cb2631bdc5559657142c1580633ead5e65ee01
aranlv <109054145+aranlv@users.noreply.github.com>
2023-10-07 21:28:45 +0700

    Sign-up page validation debug

--- script/sign-up.js
+++ script/sign-up.js
@@ -1,8 +1,10 @@
 var form = document.getElementById('form')
 
 var email = sessionStorage.getItem('toPass')
-document.getElementById('email').defaultValue = email
-sessionStorage.removeItem('toPass')
+if (email !== null) {
+  document.getElementById('email').defaultValue = email;
+  sessionStorage.removeItem('toPass');
+}
 
 var username = document.getElementById('name')
 var dob = document.getElementById('dob')
@@ -13,24 +15,32 @@ var confpassword = document.getElementById('confpassword')
 var agreement = document.getElementById('agreement')
 
 
-function checkInput(){
-  if(email.value == '' || !email.value.endsWith('@gmail.com')){
-    alert('Email must ends with @gmail.com')
-  }else if(username.value == ''){
-    alert('Name cannot be empty')
-  }else if(!male.checked && !female.checked){
-    alert('Gender must be selected')
-  }else if(password.value == '' || !checkAlphaNum(password.value)){
-    alert('Password cannot be empty and must be alphanumeric')
-  }else if(confpassword.value != password.value){
-    alert('Password doesn\'t match')
-  }else if(!agreement.checked){
-    alert('Agreement box must be checked!')
-  }else{
-      confirm('Registration Success!')
-      window.location.href = "movies.html"
+function checkInput() {
+  var emailInput = document.getElementById('email');
+
+  if (emailInput === null) {
+    console.error('Email input field not found in the DOM.');
+    return;
+  }
+
+  if (emailInput.value === '' || !emailInput.value.endsWith('@gmail.com')) {
+    alert('Email must end with @gmail.com');
+  } else if (username.value === '') {
+    alert('Name cannot be empty');
+  } else if (!male.checked && !female.checked) {
+    alert('Gender must be selected');
+  } else if (password.value === '' || !checkAlphaNum(password.value)) {
+    alert('Password cannot be empty and must be alphanumeric');
+  } else if (confpassword.value !== password.value) {
+    alert('Password doesn\'t match');
+  } else if (!agreement.checked) {
+    alert('Agreement box must be checked!');
+  } else {
+    confirm('Registration Success!');
+    window.location.href = "movies.html";
   }
 }
+
 
 function checkAlphaNum(password){
   var isNum = false
